c8e6672a88e2ea7edd1d9eeaf22ede0d9435b0ee
gagansaini1212 <gagansaini1212@gmail.com>
2019-09-03 16:27:29 +0530

    fixed drs info

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Container = styled.div`
-  margin-top: 6rem;
+  margin-top: 1rem;
   font-family: 'Raleway', sans-serif;
   background-color: #aabcbf;
   @media only screen and (max-width: 768px) {
     margin-top: 0rem;
   }
   .subtitle {
-    margin-bottom: 3rem;
+    margin: 3rem;
+    font-weight: 500;
+  }
+  .text {
+    font-size: 13px;
+    font-weight: 500;
+    letter-spacing: 1.1px;
+    line-height: 2;
+    padding: 0rem 10rem;
   }
   .button {
-    margin-top: 7rem;
+    margin-top: 4rem;
     background: transparent;
     border: none;
   }
+`;
+
+const LinkStyled = styled(AnchorLink)`
+  font-size: 14px;
 `;
 
 const About = () => (
   <Container id="about" className="container">
     <section className="section">
       <div className="has-text-centered">
-        <h2 className="subtitle is-5 has-text-white">
+        <h2 className="subtitle is-6 has-text-white">
           WHAT IS A REVERSE VENDING MACHINE?
         </h2>
         <div className="columns is-mobile is-centered">
-          <div className="column is-10">
+          <div className="column">
             <p className="has-text-white text">
               A reverse vending machine or RVM is a recycling device with
               integrated technology that accepts used (empty) beverage
@@ -38,11 +50,11 @@ const About = () => (
             <p className="has-text-white text">
               These machines provide an automated function that collects, sorts,
               compacts and stores bottles and cans. RVMs provide a simple and
-              effective solution to beverage container waste.
+              effLinkctive solution to beverage container waste.
             </p>
-            <Link to="#" className="button has-text-white">
+            <LinkStyled to="#" className="button has-text-white">
               Contact
-            </Link>
+            </LinkStyled>
           </div>
         </div>
       </div>
